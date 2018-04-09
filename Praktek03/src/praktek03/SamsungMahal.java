0d7834f162788742cc514323d73b7792a7bea749
Dendyilhamakbar <dendyilhamakbar1@gmail.com>
2018-04-09 22:19:58 +0800

    Atribut dan method

--- Praktek03/src/praktek03/SamsungMahal.java
+++ Praktek03/src/praktek03/SamsungMahal.java
@@ -1,14 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package praktek03;
-
-/**
- *
- * @author WEB DESIGN 15
- */
-public class SamsungMahal {
+public class SamsungMahal extends Samsung {
+    private String jenisSamsung;
+    
+    void throttle (){
+        System.out.println("Samsung Series S");
+    }
     
 }
