c7ced923e94becb410257d440b364691311a71e9
Dendyilhamakbar <dendyilhamakbar1@gmail.com>
2018-04-09 22:09:13 +0800

    Buat Getter and Setter

--- Praktek03/src/praktek03/Samsungaksi.java
+++ Praktek03/src/praktek03/Samsungaksi.java
@@ -3,9 +3,10 @@ package praktek03;
 public class Samsungaksi {
      public static void main(String[] args) {
         Samsung s =  new Samsung();
-        s.merk="Samsung S9";
-        s.warna="Rose Gold";
-        s.harga=15000;
+        
+        s.setMerk("Samsung");
+        s.setWarna("Rose GOld");
+        s.setHarga(15000);
         
         s.cetakInfo();
     }
