1c5d9e0f26c7a9abb4a615c22229cfac981a8bd7
Dendyilhamakbar <dendyilhamakbar1@gmail.com>
2018-04-09 22:14:22 +0800

    Menggunakan Method Get

--- Praktek03/src/praktek03/Samsungaksi.java
+++ Praktek03/src/praktek03/Samsungaksi.java
@@ -6,8 +6,15 @@ public class Samsungaksi {
         
         s.setMerk("Samsung");
         s.setWarna("Rose Gold");
-        s.setHarga(15000);
+        s.setHarga(15000); 
         
         s.cetakInfo();
+        
+        System.out.print("Merknya \t: ");
+        System.out.println(s.getMerk());
+        System.out.print("Warnanya \t: ");
+        System.out.println(s.getWarna());
+        System.out.print("Harganya \t: ");
+        System.out.println(s.getHarga());
     }
 }
