a12a42e26e1728643579d7ab624b1fe23304c2e4
Dendyilhamakbar <dendyilhamakbar1@gmail.com>
2018-04-09 21:59:56 +0800

    Instance, buat atribut dan panggil method

--- Praktek03/src/praktek03/Samsungaksi.java
+++ Praktek03/src/praktek03/Samsungaksi.java
@@ -2,6 +2,11 @@ package praktek03;
 
 public class Samsungaksi {
      public static void main(String[] args) {
+        Samsung s =  new Samsung();
+        s.merk="Samsung S9";
+        s.warna="Rose Gold";
+        s.harga=15000;
         
+        s.cetakInfo();
     }
 }
